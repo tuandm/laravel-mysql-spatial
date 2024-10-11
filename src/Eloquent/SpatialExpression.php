de666bf475b1f431dc9e81e18d34cee0f8dfedb5
Tuan Duong <bacduong@gmail.com>
2024-10-11 16:53:46 +0700

    Partial update tests

--- src/Eloquent/SpatialExpression.php
+++ src/Eloquent/SpatialExpression.php
@@ -2,15 +2,15 @@
 
 namespace Grimzy\LaravelMysqlSpatial\Eloquent;
 
+use Illuminate\Database\Grammar;
 use Illuminate\Database\Query\Expression;
 
 class SpatialExpression extends Expression
 {
-    public function getValue()
+    public function getValue(Grammar $grammar)
     {
         return "ST_GeomFromText(?, ?, 'axis-order=long-lat')";
     }
-
     public function getSpatialValue()
     {
         return $this->value->toWkt();
